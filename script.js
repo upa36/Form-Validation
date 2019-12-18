c54e80048b6c077ef3612212e47826c70255c77c
Upasana Tanvir <upasana@Upasanas-MacBook-Pro.local>
2019-12-18 14:29:24 -0500

    initial create

---
+++ script.js
@@ -0,0 +1,27 @@
+const name = document.getElementById('name')
+const passsword = document.getElementById('password')
+const form = document.getElementById('form')
+const errorElement = document.getElementById('error')
+
+form.addEventListener('submit', (e) => {
+    let messages = []
+    if (name.value === '' || name.value === null) {
+        messages.push('Name is Required')
+    }
+    if (passsword.value.length <= 6) {
+        messages.push('Password must be longer than 6 characters')
+    }
+    if (passsword.value.length >= 20) {
+        messages.push('Password must be less than 20 characters')
+    }
+
+    if (passsword.value === 'password') {
+        messages.push('Password cannot be set as password')
+    }
+
+
+    if (messages.length > 0) {
+        e.preventDefault()
+        errorElement.innerText = messages.join(', ')
+    }
+})
